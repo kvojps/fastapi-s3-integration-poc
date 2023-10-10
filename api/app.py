298302b4d63f1a1307863e9c93cf3ec325b2d456
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-09 21:13:13 -0300

    [feat]: add route to upload file

--- api/app.py
+++ api/app.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+
+from api.service.file_handler import FileHandlerService
+
+from api.adapter.file_handler_s3_adapter import FileHandlerS3Adapter
 
 app = FastAPI(
     title="API - Operações S3",
@@ -6,7 +10,15 @@ app = FastAPI(
     description="API para validação de operações S3 de entrada e saída de arquivos"
 )
 
+adapter = FileHandlerS3Adapter()
+service = FileHandlerService(adapter)
+
 
 @app.get("/health-check")
 def health_check():
     return {"status": "ok"}
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    return service.upload_file(file)
