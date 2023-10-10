1f0db244e87d377b586f9a9d577942d363484f83
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-09 21:18:06 -0300

    [fix]: add exception handler to upload operation

--- api/adapter/file_handler_s3_adapter.py
+++ api/adapter/file_handler_s3_adapter.py
@@ -1,12 +1,14 @@
 import os
 import uuid
 
-from fastapi import UploadFile
+from fastapi import UploadFile, HTTPException
 
 from api.port.file_handler import FileHandlerProvider
 
 from api.config.aws_s3 import S3ClientSession
 from api.config.dynaconf import settings
+
+from botocore.exceptions import ClientError
 
 
 class FileHandlerS3Adapter(FileHandlerProvider):
@@ -15,11 +17,24 @@ class FileHandlerS3Adapter(FileHandlerProvider):
         self._bucket_name = settings.AWS_S3_BUCKET_NAME
 
     def upload_file(self, file: UploadFile):
-        file_uuid = str(uuid.uuid4())
-        file_extension = os.path.splitext(file.filename)[1]
-        s3_object_key = f"{file_uuid}{file_extension}"
+        try:
+            file_uuid = str(uuid.uuid4())
+            file_extension = os.path.splitext(file.filename)[1]
+            s3_object_key = f"{file_uuid}{file_extension}"
 
-        self._s3_client.upload_fileobj(
-            file.file, self._bucket_name, s3_object_key)
+            self._s3_client.upload_fileobj(
+                file.file, self._bucket_name, s3_object_key)
+        except ClientError as e:
+            error_message = e.response["message"]
+            error_code = e.response["Error"]["Code"]
+            error_status_code = e.response["ResponseMetadata"]["HTTPStatusCode"]
+
+            self._logger.error(
+                f'Upload object failed'
+                f'Error code: {error_code}, Error message: {error_message}'
+            )
+
+            raise HTTPException(
+                status_code=error_status_code, detail=error_message)
 
     def create_upload_url(self): ...
