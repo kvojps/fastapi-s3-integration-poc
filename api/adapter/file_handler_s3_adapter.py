169d4b4c99b714fc45f0ecdf2d6e759a48250799
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-09 21:04:54 -0300

    [fix]: change methods name

--- api/adapter/file_handler_s3_adapter.py
+++ api/adapter/file_handler_s3_adapter.py
@@ -14,7 +14,7 @@ class FileHandlerS3Adapter(FileHandlerProvider):
         self._s3_client = S3ClientSession
         self._bucket_name = settings.AWS_S3_BUCKET_NAME
 
-    def upload(self, file: UploadFile):
+    def upload_file(self, file: UploadFile):
         file_uuid = str(uuid.uuid4())
         file_extension = os.path.splitext(file.filename)[1]
         s3_object_key = f"{file_uuid}{file_extension}"
@@ -22,4 +22,4 @@ class FileHandlerS3Adapter(FileHandlerProvider):
         self._s3_client.upload_fileobj(
             file.file, self._bucket_name, s3_object_key)
 
-    def upload_with_presigned_urls(self, file: UploadFile): ...
+    def create_upload_url(self): ...
