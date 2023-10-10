86eae93c0f7acf7e75511bc79ef5519d95c5960e
Jose Ferreira <josebazjunior09@gmail.com>
2023-10-09 22:23:16 -0300

    [fix]: remove s3config depends

--- api/config/aws_s3.py
+++ api/config/aws_s3.py
@@ -14,26 +14,20 @@ from api.config.dynaconf import settings
 class S3Config:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
-        self._session = boto3.Session(
-            aws_access_key_id=settings.MKTPLACE_AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.MKTPLACE_AWS_SECRET_ACCESS_KEY
-        )
+        self._region = settings.AWS_REGION
+        self._aws_access_key_id = settings.MKTPLACE_AWS_ACCESS_KEY_ID
+        self._aws_secret_access_key = settings.MKTPLACE_AWS_SECRET_ACCESS_KEY
 
     def s3_client(self) -> BaseClient:
         try:
-            s3 = self._session.client(
-                service_name='s3',
-                aws_access_key_id=self._access_key_id,
-                aws_secret_access_key=self._secret_access_key
+            s3 = boto3.client(
+                's3',
+                self._region,
+                aws_access_key_id=self._aws_access_key_id,
+                aws_secret_access_key=self._aws_secret_access_key
             )
-
             return s3
+
         except ClientError as e:
             self._logger.error(f'S3 Client startup error:  {e}')
             raise e
-
-    def __call__(self) -> Any:
-        self.s3_client()
-
-
-S3ClientSession = Depends(S3Config)
